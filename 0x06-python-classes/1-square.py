93faf5f6ef8ac8b1aaf89dbb137d1f309acec2ed
atrevorMatovu <atrevormatovu@gmail.com>
2023-08-29 23:45:22 -0700

    IPSUM

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Define square class"""
+
 class Square:
+    """Represent the class"""
+
     def __init__(self, size):
+        """Initialize the square class"""
+
         self.__size = size
